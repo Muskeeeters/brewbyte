9178e7dedd513b66e6a1d143eca8992d64b15ceb
Talal <muhammadtalal780@gmail.com>
2025-12-07 11:43:38 +0500

    Create menu_model.dart

---
+++ lib/models/menu_model.dart
@@ -0,0 +1,11 @@
+class MenuModel {
+  final String id;
+  final String name;
+  final String description;
+
+  MenuModel({
+    required this.id,
+    required this.name,
+    required this.description,
+  });
+}
