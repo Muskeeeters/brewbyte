de9838897c0b619c754a015152877d6d61ba1fdc
Talal <muhammadtalal780@gmail.com>
2025-12-07 11:44:06 +0500

    add screens to route

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,10 +3,15 @@ import '../pages/login_page.dart';
 import '../pages/signup_page.dart';
 import '../pages/auth_gate.dart';
 import '../pages/home_page.dart';
+import '../screens/menu_screens/menu_list_screen.dart';
+import '../screens/menu_screens/add_menu_screen.dart';
 
 final Map<String,WidgetBuilder> appRoutes = {
   '/': (context) => const AuthGate(),
   '/login':(context) => const LoginPage(),
   '/signup':(context)=> const SignupPage(),
   '/home':(context) => const HomePage(),
+
+  '/menu_list': (context) => const MenuListScreen(),
+  '/add_menu': (context) => const AddMenuScreen(),
 };
